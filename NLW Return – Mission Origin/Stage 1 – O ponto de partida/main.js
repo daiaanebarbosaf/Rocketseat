9072f110fabca5285d1059bff8e8746c44df7d16
Daiane Barbosa Farias <daiaanebarbosaf@gmail.com>
2022-05-31 09:08:07 -0300

    Update main.js

--- NLW Return – Mission Origin/Stage 1 – O ponto de partida/main.js
+++ NLW Return – Mission Origin/Stage 1 – O ponto de partida/main.js
@@ -1,17 +1,17 @@
 function onScroll() {
     if(scrollY > 0) {
-        navigation.classList.add('scroll');
+        navigation.classList.add('scroll')
     } else {
-        navigation.classList.remove('scroll');
+        navigation.classList.remove('scroll')
     }
 }
 
-function openMenu(){
-    document.body.classList.add('menu-expandided');
+function openMenu() {
+    document.body.classList.add('menu-expanded')
 }
 
-function closeMenu(){
-    document.body.classList.remove('menu-expanded');
+function closeMenu() {
+    document.body.classList.remove('menu-expanded')
 }
 
 ScrollReveal({
@@ -24,7 +24,7 @@ ScrollReveal({
 #home .stats, 
 #services,
 #services header,
-#services .cards
+#services .card
 #about,
 #about header,
-#about .content`);
+#about .content`)
